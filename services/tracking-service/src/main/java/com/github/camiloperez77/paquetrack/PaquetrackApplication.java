8288f69c13c799f20c94c0a137af32ecfa1163d0
camilop <camiloperezpadilla7@gmail.com>
2026-03-28 12:09:15 -0500

    Corrige estructura de directorios

---
+++ services/tracking-service/src/main/java/com/github/camiloperez77/paquetrack/PaquetrackApplication.java
@@ -0,0 +1,13 @@
+package com.github.camiloperez77.paquetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaquetrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaquetrackApplication.class, args);
+	}
+
+}
---
+++ services/tracking-service/src/test/java/com/github/camiloperez77/paquetrack/PaquetrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.camiloperez77.paquetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaquetrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
